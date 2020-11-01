01377d9d179c8ce09d670cff6ffc646ca526d4f4
MUHAMMAD USAMA MASOOD <71522257+Usama-Git-Hub@users.noreply.github.com>
2020-11-01 17:18:43 +0500

    Add files via upload

---
+++ sum and avg array.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int arr[5],i,sum;
+	float avg = 0.0;
+	for (i = 0;i > 5;i++)
+	{
+		cout << "enter number";
+		cin >> arr[i];
+		
+	}
+	sum = arr[i] + sum;
+
+	for (i = 0;i > 5;i++)
+	{
+		cout << "sum is" << sum << endl;
+		cout << "average is" << avg << endl;
+		
+	}
+	   avg = sum / 5;
+	return 0;
+
+
+
+
+
+}
